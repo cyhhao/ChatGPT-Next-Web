720e085a842fd6d49edafcf09378986cfbdd96a0
miracle2050 <10199095@qq.com>
2023-04-08 10:10:11 +0800

    Add files via upload

---
+++ ChatGPT-Next-Web/app/api/common.ts
@@ -0,0 +1,39 @@
+import { NextRequest } from "next/server";
+
+const OPENAI_URL = "api.openai.com";
+const DEFAULT_PROTOCOL = "https";
+const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
+const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
+
+export async function requestOpenai(req: NextRequest) {
+  const apiKey = req.headers.get("token");
+  const openaiPath = req.headers.get("path");
+
+  console.log("[Proxy] ", openaiPath);
+  let bodyText = "";
+  // if (openaiPath == "v1/chat/completions") {
+  //   // control the body params
+
+  //   const bodyData = JSON.parse(await req.text());
+  //   if (bodyData.max_tokens > 2000) {
+  //     throw Error("[Illegal] max_tokens must be less than 2000");
+  //   }
+  //   if (bodyData.model != "gpt-3.5-turbo") {
+  //     throw Error("[Illegal] model must be gpt-3.5-turbo");
+  //   }
+  //   if (bodyData.messages.length > 24) {
+  //     throw Error("[Illegal] messages length must be less than 24");
+  //   }
+
+  //   bodyText = JSON.stringify(bodyData);
+  // }
+
+  return fetch(`${PROTOCOL}://${BASE_URL}/${openaiPath}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    method: req.method,
+    body: bodyText ? bodyText : req.body,
+  });
+}
---
+++ ChatGPT-Next-Web/app/components/home.tsx
@@ -0,0 +1,281 @@
+"use client";
+
+require("../polyfill");
+
+import { useState, useEffect } from "react";
+
+import { IconButton } from "./button";
+import styles from "./home.module.scss";
+
+import SettingsIcon from "../icons/settings.svg";
+import GithubIcon from "../icons/github.svg";
+import ChatGptIcon from "../icons/chatgpt.svg";
+
+import BotIcon from "../icons/bot.svg";
+import AddIcon from "../icons/add.svg";
+import LoadingIcon from "../icons/three-dots.svg";
+import CloseIcon from "../icons/close.svg";
+import SendWhiteIcon from "../icons/send-white.svg";
+
+import { useAccessStore, useChatStore } from "../store";
+import { isMobileScreen } from "../utils";
+import Locale from "../locales";
+import { Chat } from "./chat";
+
+import dynamic from "next/dynamic";
+import { REPO_URL } from "../constant";
+import { Modal } from "./ui-lib";
+import { ErrorBoundary } from "./error";
+
+export function Loading(props: { noLogo?: boolean }) {
+  return (
+    <div className={styles["loading-content"]}>
+      {!props.noLogo && <BotIcon />}
+      <LoadingIcon />
+    </div>
+  );
+}
+
+const Settings = dynamic(async () => (await import("./settings")).Settings, {
+  loading: () => <Loading noLogo />,
+});
+
+const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
+  loading: () => <Loading noLogo />,
+});
+
+function useSwitchTheme() {
+  const config = useChatStore((state) => state.config);
+
+  useEffect(() => {
+    document.body.classList.remove("light");
+    document.body.classList.remove("dark");
+
+    if (config.theme === "dark") {
+      document.body.classList.add("dark");
+    } else if (config.theme === "light") {
+      document.body.classList.add("light");
+    }
+
+    const metaDescriptionDark = document.querySelector(
+      'meta[name="theme-color"][media]',
+    );
+    const metaDescriptionLight = document.querySelector(
+      'meta[name="theme-color"]:not([media])',
+    );
+
+    if (config.theme === "auto") {
+      metaDescriptionDark?.setAttribute("content", "#151515");
+      metaDescriptionLight?.setAttribute("content", "#fafafa");
+    } else {
+      const themeColor = getComputedStyle(document.body)
+        .getPropertyValue("--theme-color")
+        .trim();
+      metaDescriptionDark?.setAttribute("content", themeColor);
+      metaDescriptionLight?.setAttribute("content", themeColor);
+    }
+  }, [config.theme]);
+}
+
+const useHasHydrated = () => {
+  const [hasHydrated, setHasHydrated] = useState<boolean>(false);
+
+  useEffect(() => {
+    setHasHydrated(true);
+  }, []);
+
+  return hasHydrated;
+};
+
+const useAccess = () => {
+  const accessStore = useAccessStore.getState();
+  const [code, setCode] = useState(accessStore.accessCode);
+  const [close, setClose] = useState(false);
+
+  const onClose = () => {
+    setClose(true);
+    accessStore.updateCode(code);
+  };
+
+  if (accessStore.token && accessStore.token.length > 3) {
+    return;
+  }
+  if (accessStore.accessCode && accessStore.accessCode.length > 3) {
+    return;
+  }
+  if (close) {
+    return;
+  }
+
+  return (
+    <div className="modal-mask" onClick={onClose}>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <Modal
+          title={Locale.Settings.AccessCode.Placeholder}
+          actions={[
+            <IconButton
+              className={styles["ok-button"]}
+              key="ok"
+              icon={<SendWhiteIcon />}
+              noDark
+              text={"OK"}
+              onClick={onClose}
+            />,
+          ]}
+          onClose={onClose}
+        >
+          <input
+            className={styles["chat-input"]}
+            type="text"
+            style={{
+              width: "100%",
+              height: "50px",
+              padding: "0",
+              maxWidth: "100%",
+            }}
+            placeholder={Locale.Settings.AccessCode.Placeholder}
+            onChange={(e) => {
+              setCode(e.currentTarget.value);
+            }}
+            value={code}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                onClose();
+              }
+            }}
+          />
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+function _Home() {
+  const [createNewSession, currentIndex, removeSession] = useChatStore(
+    (state) => [
+      state.newSession,
+      state.currentSessionIndex,
+      state.removeSession,
+    ],
+  );
+  const chatStore = useChatStore();
+  const loading = !useHasHydrated();
+  const [showSideBar, setShowSideBar] = useState(true);
+
+  // setting
+  const [openSettings, setOpenSettings] = useState(false);
+  const config = useChatStore((state) => state.config);
+
+  useSwitchTheme();
+
+  const accessModal = useAccess();
+
+  // useEffect(() => {
+  //   document.addEventListener(
+  //     "keydown",
+  //     (e) => {
+  //       if (e.keyCode == 229) e.stopPropagation();
+  //     },
+  //     true,
+  //   );
+  // }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div
+      className={`${
+        config.tightBorder && !isMobileScreen()
+          ? styles["tight-container"]
+          : styles.container
+      }`}
+    >
+      {accessModal}
+      <div
+        className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
+      >
+        <div className={styles["sidebar-header"]}>
+          <div className={styles["sidebar-title"]}>ChatMil LIANCAN Thinktank</div>
+          <div className={styles["sidebar-sub-title"]}>
+            请关注公众号“联合参考”，发送chatmil免费获取访问码
+          </div>
+          <div className={styles["sidebar-logo"]}>
+            <ChatGptIcon />
+          </div>
+        </div>
+
+        <div
+          className={styles["sidebar-body"]}
+          onClick={() => {
+            setOpenSettings(false);
+            setShowSideBar(false);
+          }}
+        >
+          <ChatList />
+        </div>
+
+        <div className={styles["sidebar-tail"]}>
+          <div className={styles["sidebar-actions"]}>
+            <div className={styles["sidebar-action"]}>
+              <IconButton
+                icon={<SettingsIcon />}
+                onClick={() => {
+                  setOpenSettings(true);
+                  setShowSideBar(false);
+                }}
+                shadow
+              />
+            </div>
+            {/* <div className={styles["sidebar-action"]}>
+              <a href={REPO_URL} target="_blank">
+                <IconButton icon={<GithubIcon />} shadow />
+              </a>
+            </div> */}
+          </div>
+          <div>
+            <IconButton
+              icon={<AddIcon />}
+              text={Locale.Home.NewChat}
+              onClick={() => {
+                createNewSession();
+                setShowSideBar(false);
+              }}
+              shadow
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["window-content"]}>
+        {openSettings ? (
+          <Settings
+            closeSettings={() => {
+              setOpenSettings(false);
+              setShowSideBar(true);
+            }}
+          />
+        ) : (
+          <Chat
+            key="chat"
+            showSideBar={() => setShowSideBar(true)}
+            chatShowing={!showSideBar && !openSettings}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export function Home() {
+  return (
+    <ErrorBoundary>
+      <_Home></_Home>
+    </ErrorBoundary>
+  );
+}
